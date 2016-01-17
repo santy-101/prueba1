4c3eb08eda74c7210027b1b8af9997e2b6154405
santy-101 <santiago.lema01@epn.edu.ec>
2016-01-17 18:23:52 -0500

    Version 1.0

---
+++ Prueba/src/prueba/Prueba.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author Santy
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x;
+        int y;
+    }
+    
+}
